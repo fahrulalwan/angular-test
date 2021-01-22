6ecbe70114c90df339081db030412be0a4a8eaa6
fahrulalwan <fahrulalwan@gmail.com>
2021-01-22 20:09:39 +0700

    tailwind css initialization

---
+++ ng-tailwind.js
@@ -0,0 +1,23 @@
+module.exports = {
+  // Tailwind Paths
+  configJS: '/home/malwan/Documents/Workspaces/Personal/angular-test/tailwind.config.js',
+  sourceCSS: '/home/malwan/Documents/Workspaces/Personal/angular-test/src/tailwind.css',
+  outputCSS: '/home/malwan/Documents/Workspaces/Personal/angular-test/src/tailwind-output.css',
+  // Sass
+  sass: false,
+  // PurgeCSS Settings
+  purge: false,
+  keyframes: false,
+  fontFace: false,
+  rejected: false,
+  whitelist: [],
+  whitelistPatterns: [],
+  whitelistPatternsChildren: [],
+  extensions: [
+    '.ts',
+    '.html',
+    '.js'
+  ],
+  extractors: [],
+  content: []
+}
