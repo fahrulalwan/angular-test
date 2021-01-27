f9cad85eac39a1ead186394ad1a73f139b9a0acb
fahrulalwan <fahrulalwan@gmail.com>
2021-01-27 14:17:44 +0700

    beautify data fetching

--- src/app/pages/all-ticket/all-ticket.component.ts
+++ src/app/pages/all-ticket/all-ticket.component.ts
@@ -1,27 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AppService } from '../../service/app.service';
 import { Observable } from 'rxjs';
 import { Datasource, Filter } from '../../types/datasource';
+import { startWith, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-all-ticket',
   templateUrl: './all-ticket.component.html',
   styleUrls: ['./all-ticket.component.css']
 })
-export class AllTicketComponent implements OnInit {
-  dummyData$: Observable<Datasource[]> = this.appService.getAllTicket();
+export class AllTicketComponent {
+  private dummyData: Datasource[] = [];
+  dummyData$: Observable<Datasource[]> = this.appService.getAllTicket().pipe(tap(val => this.dummyData = val));
 
   constructor(public appService: AppService) {
   }
 
-  ngOnInit() {
-  }
-
-  onFilterForm($event: { filterBy: Filter, filterValue: string }) {
-    this.dummyData$ = this.appService.getAllTicket($event.filterBy, $event.filterValue);
-  }
-
-  resetFilter() {
-    this.dummyData$ = this.appService.getAllTicket();
+  onFilterForm($event?: { filterBy: Filter, filterValue: string }) {
+    this.dummyData$ = this.appService.getAllTicket($event?.filterBy, $event?.filterValue).pipe(
+      startWith(this.dummyData),
+      tap(val => this.dummyData = val)
+    );
   }
 }
